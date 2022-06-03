47c369fd85168b4ef339a64a754806c69bb9bb7d
jesusdiaz <jesusdiazh11@gmail.com>
2022-06-03 02:59:42 -0500

    Updates

---
+++ precache-manifest.d88982dbfce95d11de4b3c80a4eb40d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7c69d108b240f76e5be7",
+    "url": "/RUBRICA-CORTE-3/static/css/main.d05afdbf.chunk.css"
+  },
+  {
+    "revision": "7c69d108b240f76e5be7",
+    "url": "/RUBRICA-CORTE-3/static/js/main.7c69d108.chunk.js"
+  },
+  {
+    "revision": "bf7cc9647dc8abc7f85d",
+    "url": "/RUBRICA-CORTE-3/static/js/1.bf7cc964.chunk.js"
+  },
+  {
+    "revision": "a49ff047d9ce0a29d1f3",
+    "url": "/RUBRICA-CORTE-3/static/js/runtime~main.a49ff047.js"
+  },
+  {
+    "revision": "d6a34c56e4d002a18ea29ff5e4bf45c6",
+    "url": "/RUBRICA-CORTE-3/static/media/inicio1.d6a34c56.png"
+  },
+  {
+    "revision": "b8bc85eaa650655fcf7cc039d90488d8",
+    "url": "/RUBRICA-CORTE-3/index.html"
+  }
+];
